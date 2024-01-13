b8bb570084e58bb7f5a745329390c2527b1e1374
荒木勇人 <arakiyuuto@arakihayatonoMacBook-Air.local>
2024-01-13 17:08:24 +0900

    何かしらのコメント

--- resources/views/regist.blade.php
+++ resources/views/regist.blade.php
@@ -13,41 +13,31 @@
                     <div class="regist-form">
                         <div id="id-area">
                             <label for="" class="form-label">id</label>
-                            <p>{{ $companies }}</p>
+                            
                         </div>
                         <div id="name-area">
                             <label for="" class="form-label">商品名</label>
                             <input type="text" name="product_name" value="{{ $companies }}">
-                            @if($errors->has('product_name'))
-                                <p>{{ $errors->first('product_name') }}</p>
-                            @endif
+                            
                         </div>
                         <div id="company-area">
                             <label for="" class="form-label">メーカー</label>
-                            @if($errors->has('company_id'))
-                                <p>{{ $errors->first('company_id') }}</p>
-                            @endif
+                            
                         </div>
                         <div id="price-area">
                             <label for="" class="form-label">価格</label>
                             <input type="text" name="price" value="{{ $companies }}">
-                            @if($errors->has('price'))
-                                <p>{{ $errors->first('price') }}</p>
-                            @endif
+                            
                         </div>
                         <div id="stock-area">
                             <label for="" class="form-label">在庫</label>
                             <input type="text" name="stock" value="{{ $companies }}">
-                            @if($errors->has('stock'))
-                                <p>{{ $errors->first('stock') }}</p>
-                            @endif
+                            
                         </div>
                         <div id="comment-area">
                             <label for="" class="form-label">コメント</label>
                             <textarea name="comment" id="" cols="30" rows="2">{{ $companies }}</textarea>
-                            @if($errors->has('comment'))
-                                <p>{{ $errors->first('comment') }}</p>
-                            @endif
+                            
                         </div>
                         <div id="old-img-area">
                             <img src="{{ asset('$companies->img_path') }}" alt="商品画像" class="img_view">
